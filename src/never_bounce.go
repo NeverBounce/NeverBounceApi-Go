575b646e3057880d990d8a5c110f28d424b19828
ayman <enkhalifapro@gmail.com>
2017-07-24 01:33:53 +0200

    wrap info API

--- src/never_bounce.go
+++ src/never_bounce.go
@@ -7,6 +7,7 @@ import (
 	"errors"
 	"github.com/enkhalifapro/neverBounce/src/nb_error"
 	"github.com/enkhalifapro/neverBounce/src/nb_dto"
+	"fmt"
 )
 
 type NeverBounce struct {
@@ -16,10 +17,17 @@ type NeverBounce struct {
 func New(apiKey string) (error, *NeverBounce) {
 	r := &NeverBounce{}
 	r.ApiKey = apiKey
+	err, _ := r.Info()
+	if err != nil {
+		return err, nil
+	}
 	return nil, r
 }
 
+// Account endpoints allow to programmatically check your account's balance and
+// how many jobs are currently running on your account.
 func (r *NeverBounce) Info() (error, *nbDto.Info) {
+	// call info API
 	url := "https://api.neverbounce.com/v4/account/info?key=" + r.ApiKey
 	res, err := http.Get(url)
 	if err != nil {
@@ -31,6 +39,8 @@ func (r *NeverBounce) Info() (error, *nbDto.Info) {
 	if err != nil {
 		return err, nil
 	}
+
+	// check error response
 	var authError nbError.AuthError
 
 	err = json.Unmarshal(body, &authError)
@@ -41,6 +51,7 @@ func (r *NeverBounce) Info() (error, *nbDto.Info) {
 		return errors.New(authError.Message), nil
 	}
 
+	// check success response
 	var info nbDto.Info
 
 	err = json.Unmarshal(body, &info)
