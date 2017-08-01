ed0e8ee90c2be32723bfb81fd6c127385fa08d08
ayman <enkhalifapro@gmail.com>
2017-08-01 18:49:09 +0200

    wrap job results API

--- src/nb_dto/result_info.go
+++ src/nb_dto/result_info.go
@@ -13,14 +13,17 @@ type ResultQuery struct {
 	ItemsPerPage int `json:"items_per_page"`
 }
 
-// Data : Email data of Result API
+// Verification : The data object will contain the original data submitted for this row.
+// If the source data was submitted via the API the data will have the same keys as when it was originally submitted.
+// If submitted as a CSV in the dashboard the data will use the header row to determine the keys for the data when available.
 type Data struct {
 	Email string `json:"email"`
 	ID    string `json:"id"`
 	Name  string `json:"name"`
 }
 
-// Verification : Email verification data of Result API
+// Verification : The verification object contains the verification results for the email being verified.
+// This object contains the same information that the single/check API returns.
 type Verification struct {
 	Result              string `json:"result"`
 	Flags               []string `json:"flags"`
